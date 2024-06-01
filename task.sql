4bae12baa0fedb4eccfb33374711c4babd2dc65b
alexk <alexkochubey13@gmail.com>
2024-06-01 13:45:47 +0300

    Solution?

--- task.sql
+++ task.sql
@@ -39,7 +39,7 @@ CREATE TABLE ProductInventory (
 );
 --rollback DROP TABLE ProductInventory;
 
---changeset mate.acamemy:5 labels:0.0.2
+--changeset o.kochubei:5 labels:0.0.2
 CREATE TABLE Users (
     ID INT AUTO_INCREMENT,
     FirstName VARCHAR(50),
@@ -49,6 +49,6 @@ CREATE TABLE Users (
 );
 --rollback DROP TABLE Users;
 
---changeset mate.acamemy:6 labels:0.0.3
+--changeset o.kochubei:6 labels:0.0.3
 CREATE INDEX idx_email ON Users (Email);
---rollback ALTER TABLE Users DROP INDEX idx_email;
+--rollback DROP INDEX idx_email ON Users;
